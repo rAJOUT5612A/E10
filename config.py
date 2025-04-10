19901bd59069e4057a568482cf185679ccd5c86e
rAJOUT5612A <lena12q@outlook.com>
2025-04-10 07:15:45 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_TOKEN = "7101538144:AAH_-ZwUTRZdiBLm-WtiOKg4_K2xgL2L-Jk"  # Get from @BotFather
+ADMIN_ID = ["1821595166"]      # Your Telegram user ID
+CHANNEL_USERNAME = "@RAJPUTDDOSSS"  # Channel users must join
+FEEDBACK_CHANNEL = "@RAJPUTJIOPA"  # Where screenshots go
+ALLOWED_GROUPS = ["-1002499192449"]  # Default group ID
